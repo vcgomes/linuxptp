55b58e9ddc70f146d5fe479b9e59d80dcde6381b
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2021-02-25 10:38:58 -0800

    sysoff: Add support for retrieving cross timestamps
    Use the newly introduced ioctl() to enable and then retrieve cross
    timestamp events from the kernel.
    
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>

--- sysoff.c
+++ sysoff.c
@@ -19,6 +19,7 @@
  */
 #include <stdio.h>
 #include <string.h>
+#include <poll.h>
 #include <sys/ioctl.h>
 #include <linux/ptp_clock.h>
 
@@ -26,10 +27,17 @@
 #include "sysoff.h"
 
 #define NS_PER_SEC 1000000000LL
+#define NS_PER_MSEC 1000000LL
 
 static int64_t pctns(struct ptp_clock_time *t)
 {
 	return t->sec * NS_PER_SEC + t->nsec;
+}
+
+static void nspct(int64_t t, struct ptp_clock_time *pct)
+{
+	pct->sec = t / NS_PER_SEC;
+	pct->nsec = t % NS_PER_SEC;
 }
 
 static int sysoff_precise(int fd, int64_t *result, uint64_t *ts)
@@ -43,6 +51,60 @@ static int sysoff_precise(int fd, int64_t *result, uint64_t *ts)
 	*result = pctns(&pso.sys_realtime) - pctns(&pso.device);
 	*ts = pctns(&pso.sys_realtime);
 	return SYSOFF_PRECISE;
+}
+
+static int sysoff_cross(int fd, int64_t *result, uint64_t *ts, int64_t *delay)
+{
+	struct pollfd pfd = { fd, POLLIN, 0 };
+	struct ptp_extts_event_cross *event = NULL;
+	static char buf[4096];
+	ssize_t ret;
+	int num;
+
+	ret = poll(&pfd, 1, 0);
+	if (ret < 1) {
+		pr_debug("poll sysoff_cross: %m");
+		return SYSOFF_RUN_TIME_MISSING;
+	}
+
+	ret = read(fd, buf, sizeof(buf));
+	if (ret < 0) {
+		pr_debug("read sysoff_cross: %m");
+		return SYSOFF_RUN_TIME_MISSING;
+	}
+
+	if (ret < sizeof(*event)) {
+		pr_debug("invalid event size");
+		return SYSOFF_RUN_TIME_MISSING;
+	}
+
+	num = ret / sizeof(*event);
+	event = (struct ptp_extts_event_cross *) &buf[(num - 1) * sizeof(*event)];
+
+	if (!(event->flags & PTP_EVENT_CROSS)) {
+		pr_debug("invalid event flags");
+		return SYSOFF_RUN_TIME_MISSING;
+	}
+
+	*result = pctns(&event->t) - event->tstamp;
+	*ts = pctns(&event->t);
+	*delay = PTP_EVENT_CROSS_DELAY(event->flags);
+
+	return SYSOFF_CROSS;
+}
+
+static int sysoff_cross_enable(int fd, int period)
+{
+	struct ptp_crossts_request cross;
+	memset(&cross, 0, sizeof(cross));
+
+	nspct(period, &cross.period);
+	if (ioctl(fd, PTP_CROSSTS_REQUEST, &cross)) {
+		pr_debug("ioctl PTP_CROSSTS_REQUEST: %m");
+		return SYSOFF_RUN_TIME_MISSING;
+	}
+
+	return SYSOFF_CROSS;
 }
 
 static int64_t sysoff_estimate(struct ptp_clock_time *pct, int extended,
@@ -122,6 +184,9 @@ int sysoff_measure(int fd, int method, int n_samples,
 		   int64_t *result, uint64_t *ts, int64_t *delay)
 {
 	switch (method) {
+	case SYSOFF_CROSS:
+		*delay = 0;
+		return sysoff_cross(fd, result, ts, delay);
 	case SYSOFF_PRECISE:
 		*delay = 0;
 		return sysoff_precise(fd, result, ts);
@@ -137,6 +202,7 @@ int sysoff_probe(int fd, int n_samples)
 {
 	int64_t junk, delay;
 	uint64_t ts;
+	int period = 1 * NS_PER_MSEC;
 	int i;
 
 	if (n_samples > PTP_MAX_SAMPLES) {
@@ -146,7 +212,10 @@ int sysoff_probe(int fd, int n_samples)
 		return SYSOFF_RUN_TIME_MISSING;
 	}
 
-	for (i = 0; i < SYSOFF_LAST; i++) {
+	if (sysoff_cross_enable(fd, period) >= 0)
+		return SYSOFF_CROSS;
+
+	for (i = SYSOFF_CROSS + 1; i < SYSOFF_LAST; i++) {
 		if (sysoff_measure(fd, i, n_samples, &junk, &ts, &delay) < 0)
 			continue;
 		return i;
